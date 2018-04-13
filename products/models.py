ed29e511a011458748d2f6f2bd919cfd51cf1efc
GdRaymond <gd.raymond@gmail.com>
2018-04-13 16:52:55 +1000

    update create order from trace

--- products/models.py
+++ products/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Fabric(models.Model):
+    fabric=models.TextField(max_length=200)
+
+    class Meta:
+        verbose_name='Fabric'
+        verbose_name_plural='Fabrics'
+
 class Product(models.Model):
 
     style_no=models.TextField(max_length=50)
+    client=models.TextField(max_length=100,null=True,blank=True)
     commodity=models.TextField(max_length=100,null=True,blank=True)
-    fabric=models.TextField(max_length=100,null=True,blank=True)
+    fabric=models.ForeignKey(Fabric,on_delete=models.PROTECT)
     fabric_usage=models.DecimalField(max_digits=4,decimal_places=2,default=0)
     quantity_per_carton=models.IntegerField(default=0)
     volume_per_carton=models.DecimalField(max_digits=4,decimal_places=3,default=0)
